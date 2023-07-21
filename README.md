0d685fa9015be140b9082ce9769f64c25dc57e81
Mateo Maldonado Tolentino <matt.maldonado.tolentino@gmail.com>
2023-07-20 23:17:48 -0600

    ddi practica7 200906

---
+++ README.md
@@ -0,0 +1,24 @@
+# DDI_Practica7_200448
+
+[![Banner-de-Twitch-Nubes-Gamer-Chica-Morado.png](https://i.postimg.cc/15q3LFXF/Banner-de-Twitch-Nubes-Gamer-Chica-Morado.png)](https://postimg.cc/MvzwBvyZ)
+
+<div align="center">
+  
+# Universidad Tecnológica de Xicotepec de Juárez
+
+
+## Ingeniería en Desarrollo y Gestión de Software
+## Gabriel Guzmán Garcia
+## 9A
+### Desarrollo de Dispositivos Inteligentes
+
+
+
+
+&nbsp;
+&nbsp;
+|  Característica |  Información |
+| :------------: | :------------: |
+| Nombre | **Practica 7** *Saludo en base a la hora* |
+| Descripción  | En resumen, la aplicación "Saludo Hora" para Wear OS es una aplicación interactiva que genera saludos personalizados basados en la hora actual. Con su diseño atractivo y su capacidad para adaptarse a diferentes momentos del día, brinda a los usuarios una experiencia amigable y agradable en su reloj inteligente. |
+|  Fecha de Carga | 07/06/23  |
---
+++ app/src/main/java/mx/edu/utxj/tidgs/ddi/practica6_200931/presentation/MainActivity.kt
@@ -0,0 +1,84 @@
+/* While this template provides a good starting point for using Wear Compose, you can always
+ * take a look at https://github.com/android/wear-os-samples/tree/main/ComposeStarter and
+ * https://github.com/android/wear-os-samples/tree/main/ComposeAdvanced to find the most up to date
+ * changes to the libraries and their usages.
+ */
+
+
+package mx.edu.utxj.tidgs.ddi.practica6_200931.presentation
+
+import android.os.Bundle
+import android.os.Handler
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
+import mx.edu.utxj.tidgs.ddi.practica6_200931.R
+import mx.edu.utxj.tidgs.ddi.practica6_200931.presentation.theme.Practica6_200931Theme
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+
+class MainActivity : ComponentActivity() { // Reemplaza con el nombre de tu Activity
+    private lateinit var clockTextView: TextView
+    private lateinit var saludoTextView: TextView
+    private lateinit var handler: Handler
+    private lateinit var updateTimeRunnable: Runnable
+    private var mHandler: Handler? = null
+    private var mRunnable: Runnable? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        clockTextView = findViewById(R.id.clockTextView)
+        saludoTextView = findViewById(R.id.saludo)
+        val calendar = Calendar.getInstance()
+        val hourOfDay = calendar.get(Calendar.HOUR_OF_DAY)
+        val saludo: String = when(hourOfDay) {
+            in 4..12 -> "Buenos dias!"
+            in 12..20 -> "Buenas tardes!"
+            else -> "Buenas noches!"
+        }
+
+        saludoTextView.text = saludo
+
+        handler = Handler()
+        updateTimeRunnable = object : Runnable {
+            override fun run() {
+                val currentTime = Calendar.getInstance().time
+                val dateFormat =  SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(Date())
+                val formattedTime = dateFormat.format(currentTime)
+                clockTextView.text = formattedTime
+
+                // Se actualiza después de 1 segundo
+                saludoTextView.text = saludo
+                handler.postDelayed(this, 1000)
+            }
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        handler.post(updateTimeRunnable)
+    }
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(updateTimeRunnable)
+    }
+}
